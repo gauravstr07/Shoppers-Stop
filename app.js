7c545c353c4c50254ef4d21a190dfccac7acb522
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-27 01:21:23 +0530

    Sessions And Cookies

--- app.js
+++ app.js
@@ -3,24 +3,44 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MongoDB_URI = "mongodb://localhost:27017/shoppers-stop";
+
 const app = express();
 const port = 5000;
+const store = new MongoDBStore({
+  uri: MongoDB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoute = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById("6381e0433d802d2e410cfe9a")
+  if (!req.session.user) {
+   return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -30,11 +50,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoute);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect("mongodb://localhost:27017/shoppers-stop")
+  .connect(MongoDB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
